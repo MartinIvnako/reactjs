6b5e7d6edbb14c833e571e12273ab74321052d48
Martin Ivanko <martin.ivanko1238@gmail.com>
2019-07-15 14:16:57 +0200

    create root component, first render

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,34 @@
-    
-console.log("It works!");
+import React from "react";
+import { render } from "react-dom";
+
+// create components
+class App extends React.Component {     // root compoment
+    render() { // render method
+
+        // vrat mi vyrenderovane
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Hello!</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// metoda render zoberie classsu app, napis kde a zrejnderuj ju
+render(<App/>, window.document.getElementById("app"));
+
+
+
+
+
+
+
+
+
+
+
+// extend - rozsirenie
